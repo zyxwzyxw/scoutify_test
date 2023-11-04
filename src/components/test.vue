f0c7955b209e1592de5a244d3b0bf581a93fadf6
1834423612 <1834423612@qq.com>
2023-11-04 18:48:14 -0400

    refactor: Calculate Functions

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,59 +1,144 @@
-<template></template>
+<template>
+    <div class="card">
+        <div class="card-inner">
+            <h2>Game Scoring System</h2>
+            <div>
+                <label for="auton-time">Auton Time (seconds): </label>
+                <input id="auton-time" type="number" v-model.number="auton" />
+            </div>
+            <div>
+                <label for="teleop-time">Teleop Time (seconds): </label>
+                <input id="teleop-time" type="number" v-model.number="teleop" />
+            </div>
+            <div>
+                <label for="endgame-time">Endgame Time (seconds): </label>
+                <input id="endgame-time" type="number" v-model.number="endgame" />
+            </div>
+            <button @click="calculateScores">Calculate Scores</button>
+            <!-- <div v-if="scoreCalculated">
+            <h3>Scores:</h3>
+            <p>Auton Score: {{ testscores[0] }}</p>
+            <p>Teleop Score: {{ testscores[1] }}</p>
+            <p>Endgame Score: {{ testscores[2] }}</p>
+            <p>Total Score: {{ score }}</p>
+        </div> -->
+        </div>
+    </div>
+
+    <!-- Result Card -->
+    <div v-if="scoreCalculated" class="result-card">
+        <h3>Calculated Scores:</h3>
+        <p>Auton Score: {{ testscores[0] }}</p>
+        <p>Teleop Score: {{ testscores[1] }}</p>
+        <p>Endgame Score: {{ testscores[2] }}</p>
+        <p>Total Score: {{ score }}</p>
+    </div>
+</template>
 
 <script setup>
-// variables for a year by year basis
+import { ref } from 'vue';
+
+// Scoring configuration constants
 const autonscoring = {
-    time: [5], points: [2], limit: [999999],
-    // for parking, use [seconds, points] arrays
-    // leave [0, 0] option open for not parking
-    parking: [[0, 0], [3, 3]]
-}
-const teleopscoring = { time: [1], points: [1], limit: [999999] }
-const endgamescoring = { time: [1, 10], points: [1, 20], limit: [999999] }
-// variables from here on won't be changed on year by year basis
-// auton might be (15 + 3) seconds long
-let auton = 15; let teleop = 105; let endgame = 30;
-var score = 0;
-var testscores = [0, 0, 0, 0, 0];
-var points, time, limit;
+    time: [5],
+    points: [2],
+    limit: [999999],
+    parking: [
+        [0, 0],
+        [3, 3],
+    ],
+};
 
-// park calc: do a calc for each park option and not parking
-let a = autonscoring;
-let ap = autonscoring.parking;
-for (let i = 0; i < ap.length; i++) {
-    if (ap[i][0] <= auton) {
-        auton -= ap[i][0];
-        testscore[0] += ap[i][1];
-        let scoringrates = [];
-        for (let j = 0; j < a.time.length; j++) { scoringrates[j] = a.points / a.time; }
+const teleopscoring = {
+    time: [1],
+    points: [1],
+    limit: [999999]
+};
+
+const endgamescoring = {
+    time: [1, 10],
+    points: [1, 20],
+    limit: [999999]
+};
+
+// Reactivity enabled variables
+let auton = ref(15);
+let teleop = ref(105);
+let endgame = ref(30);
+let score = ref(0);
+let testscores = ref([0, 0, 0]);
+let scoreCalculated = ref(false);
+
+// Calculate Scores function
+const calculateScores = () => {
+    // Reset scores
+    score.value = 0;
+    testscores.value.fill(0);
+
+    // Auton calculations
+    let autonRemainingTime = auton.value;
+    for (let [parkTime, parkPoints] of autonscoring.parking) {
+        if (parkTime <= autonRemainingTime) {
+            autonRemainingTime -= parkTime;
+            testscores.value[0] += parkPoints;
+            break; // Assuming only one parking action can be taken
+        }
     }
-    // sort function to get highest rate scoringrates.forEach();
-    // take max and check for limitations
-    // subtract time and add points
-    // if limit reached, see how much time is left, then check for next best scoring method
+
+    // Assuming a simple scoring model where each action takes a set amount of time and scores points
+    // Additional logic would be needed for more complex scoring mechanisms
+    let autonPointsPerSecond = autonscoring.points[0] / autonscoring.time[0];
+    testscores.value[0] += autonRemainingTime * autonPointsPerSecond;
+
+    // Teleop calculations
+    let teleopPointsPerSecond = teleopscoring.points[0] / teleopscoring.time[0];
+    testscores.value[1] = teleop.value * teleopPointsPerSecond;
+
+    // Endgame calculations
+    let endgamePointsPerSecond = endgamescoring.points.reduce((acc, points, index) => {
+        return acc + (points / endgamescoring.time[index]);
+    }, 0);
+    testscores.value[2] = endgame.value * endgamePointsPerSecond;
+
+    // Calculate total score
+    score.value = testscores.value.reduce((acc, current) => acc + current, 0);
+
+    // Update the calculated flag
+    scoreCalculated.value = true;
+};
+
+</script>
+
+<style>
+.card {
+    display: flex;
+    text-align: center;
+    margin: auto;
+    background-color: aliceblue;
+    border-radius: 15px;
+    width: 50%;
+    height: 35%;
+    flex-direction: column;
+    justify-content: center;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
-// teleopcalculations
-let t = teleopscoring;
-let scoringrates = [];
-for (let i = 0; i < t.time.length; i++) { scoringrates[i] = t.points / t.time; }
+.card-inner {
+    margin: 15px;
+    overflow: auto;
+}
 
-
-// sort function to get highest rate scoringrates.forEach();
-// take max and check for limitations
-// subtract time and add points
-// if limit reached, see how much time is left, then check for next best scoring method-->might require some form of recursion
-
-// endgame calculations
-let e = endgamescoring;
-scoringrates = [];
-for (let i = 0; i < e.time.length; i++) { scoringrates[i] = e.points / e.time; }
-
-
-// sort function to get highest rate scoringrates.forEach();
-// take max and check for limitations
-// subtract time and add points
-// if limit reached, see how much time is left, then check for next best scoring method-->might require some form of recursion
-
-// some form of output
-</script>
+.result-card {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    margin: 10%;
+    margin-top: 20px;
+    padding: 15px;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
