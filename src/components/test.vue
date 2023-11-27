46bbc08c98c6df98d372888ce28b3af4ff49d543
zyxwzyxw <149740824+zyxwzyxw@users.noreply.github.com>
2023-11-27 17:07:28 -0500

    this would work except the function 'decodescenario' doesn't  work

--- src/components/test.vue
+++ src/components/test.vue
@@ -33,87 +33,153 @@
         <p>Endgame Score: {{ testscores[2] }}</p>
         <p>Total Score: {{ score }}</p>
     </div>
+
+<canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+<canvas id="rnkpntChart" style="width:100%;max-width:700px"></canvas>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+//don't know if this script can be inside the script
+<script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
-// Scoring configuration constants
-const autonscoring = {
-    time: [5],
-    points: [2],
-    limit: [999999],
+//Assumptions: We will always engage. We will score for high, then mid, then low, but because we never will get to the low grid, we don't have to account for it
+let balancingtime = [3, 5, 7];//it can take either 3 seconds, 5 seconds, or 7 seconds
+let pointsforbalancing = [12, 10];//auton and teleop
+let placetime = [2, 4];
+let pointsforhigh = [6, 5];//auton and teleop
+let pointsformid = [4, 3];
+let autonpickup = [2, 4];
+let autonmovepartofcycle = [4,6];
+let movepartofcycle =  [7, 9, 11];
+const autonPieceLimit = 5;
+const scenariospoints=new Array(balancingtime.length).fill(new Array(placetime.length).fill(new Array(autonpickup.length).fill(new Array(autonmovepartofcycle.length).fill(new Array(movepartofcycle.length).fill(new Array(0))))));//for chart
+const rnkValues=[{x:0,y:0}];
+let decodescenario = (a) =>
+a.charAt(0)*placetime.length*autonpickup.length*autonmovepartofcycle.length*movepartofcycle.length+
+balancingtime.length*a.charAt(1)*autonpickup.length*autonmovepartofcycle.length*movepartofcycle.length+
+balancingtime.length*placetime.length*a.charAt(2)*autonmovepartofcycle.length*movepartofcycle.length+
+balancingtime.length*placetime.length*autonpickup.length*a.charAt(3)*movepartofcycle.length+
+balancingtime.length*placetime.length*autonpickup.length*autonmovepartofcycle.length*a.charAt(4);;
+//create a scenario tree
+for (let i in balancingtime) {
+    for (let j in placetime) {
+        for (let k in autonpickup) {
+            for (let l in autonmovepartofcycle) {
+                for (let m in movepartofcycle) {
+//initilize the variables
+let nodesscored=0;
+let autonPCCount = 0;
+let holdingpiece=1;//start w/ holding one piece
 
-    // For parking, use [seconds, points] arrays
-    // l=Leave [0, 0] option open for not parking
-    parking: [
-        [0, 0],
-        [3, 3],
-    ],
-};
+//auton balance
+let points=pointsforbalancing[0]+3;//the three is for mobility
+let time=150-balancingtime[i];
+scenariospoints[i][j][k][l][m].push({x: 150-time, y: points});
 
-const teleopscoring = {
-    time: [1],
-    points: [1],
-    limit: [999999]
-};
-
-const endgamescoring = {
-    time: [1, 10],
-    points: [1, 20],
-    limit: [999999]
-};
-
-// Reactivity enabled variables
-// Variables from here on won't be changed on year by year basis
-// Auton might be (15 + 3) seconds long
-let auton = ref(15);
-let teleop = ref(105);
-let endgame = ref(30);
-let score = ref(0);
-let testscores = ref([0, 0, 0]);
-let scoreCalculated = ref(false);
-
-// Calculate Scores function
-const calculateScores = () => {
-    // Reset scores
-    score.value = 0;
-    testscores.value.fill(0);
-
-    // Auton calculations
-    let autonRemainingTime = auton.value;
-    for (let [parkTime, parkPoints] of autonscoring.parking) {
-        if (parkTime <= autonRemainingTime) {
-            autonRemainingTime -= parkTime;
-            testscores.value[0] += parkPoints;
-            break; // Assuming only one parking action can be taken
+//place pieces during auton
+while (time - placetime[j] >= 135 && autonPCCount < autonPieceLimit) {
+    points+=pointsforhigh[0]*holdingpiece;
+    ++nodesscored;
+	if (nodesscored % 3===0){points+=5;}
+    time -= placetime[j];
+	autonPCCount+=holdingpiece;
+    holdingpiece = 0;
+	scenariospoints[i][j][k][l][m].push({x: 150-time, y: points});
+    if (time - autonmovepartofcycle[l]/2 - autonpickup[k] >= 135) {//the autonmovepartofcycle[l] is divided by two b/c the robot only travels to pick up the cones, not also back 
+        time -= autonmovepartofcycle[l]/2 + autonpickup[k];
+        ++holdingpiece;
+        if(time-autonmovepartofcycle[l]/2-placetime[j]>=135){time-=autonmovepartofcycle[l]/2;}
+        else{time=135;}
+    }
+}
+//endgame
+time = 135 - balancingtime[i];
+points += pointsforbalancing[1];
+scenariospoints[i][j][k][l][m].push({x: 150-time, y: points});
+//teleop
+// if holding pieces at beginning of teleop
+if (holdingpiece>0) {
+    ++nodesscored;
+    points+=pointsforhigh[1]*holdingpiece;
+    holdingpiece = 0;
+    time-= placetime[j];
+    if (nodesscored % 3===0){points+=5;}//test for links
+	scenariospoints[i][j][k][l][m].push({x: 150-time, y: points});
+}
+//score high and mid
+while (time-movepartofcycle[l]-placetime[j]>=0) {
+    ++nodesscored;
+    time-=placetime[j]+movepartofcycle[l]+autonpickup[k];
+    if(nodesscored<=9){points += pointsforhigh[1];}
+    else{points+= pointsformid[1];}
+    if (nodesscored % 3===0){points +=5;}
+    scenariospoints[i][j][k][l][m].push({x: 150-time, y: points});
+}
+let x=decodescenario(''+i+j+k+l+m);
+if (Math.floor(nodesscored / 3) > 3) {rnkValues.push({x:x,y:1})}
+else{rnkValues.push({x:x,y:0})}
+                }
+            }
         }
     }
+}
+const xyValues =[];
 
-    // Assuming a simple scoring model where each action takes a set amount of time and scores points
-    // Additional logic would be needed for more complex scoring mechanisms
-    let autonPointsPerSecond = autonscoring.points[0] / autonscoring.time[0];
-    testscores.value[0] += autonRemainingTime * autonPointsPerSecond;
+//figure out which scenario each scatterpoint belongs to by defining the endpoints of each data set
+const endpoints=[-1];//its -1 b/c its the ending-point in a scenario where nothing happens: it was done like this so the next scenario starts w/ zero (-1+1=0). I added one b/c the endpoint of one scenario is 1 plus the startpoint of another scenario.
+for(let i=0;i<scenariospoints[0][0][0][0][0].length-1;i++){
+	if(scenariospoints[0][0][0][0][0][i].x>scenariospoints[0][0][0][0][0][i+1].x){endpoints.push(i);}
+}
+endpoints.push(scenariospoints[0][0][0][0][0].length);
+//i just need to be able to turn const scenario='[0][0][0][0][0][0]'; into a number
+//how can i make const a=1 if scenario is '11111', make a =2 is scenario='11112', make a=3 if scenario='11121' and so forth? Here are the constraints: the first & third # can be either 1,2 or 3 while the other #s have to be 1 or 2
 
-    // Teleop calculations
-    let teleopPointsPerSecond = teleopscoring.points[0] / teleopscoring.time[0];
-    testscores.value[1] = teleop.value * teleopPointsPerSecond;
+const scenario='00000';
 
-    // Endgame calculations
-    let endgamePointsPerSecond = endgamescoring.points.reduce((acc, points, index) => {
-        return acc + (points / endgamescoring.time[index]);
-    }, 0);
-    testscores.value[2] = endgame.value * endgamePointsPerSecond;
+const b=decodescenario(scenario);
+const startpoint=endpoints[b]+1;//an accesser
+const endpoint=endpoints[b+1];
+//for each point in the given range, push a point to the xyValues array
+for(let i=startpoint;i<endpoint;i++){xyValues.push(scenariospoints[0][0][0][0][0][i]);}
 
-    // Calculate total score
-    score.value = testscores.value.reduce((acc, current) => acc + current, 0);
-
-    // Update the calculated flag
-    scoreCalculated.value = true;
-};
-
+window.addEventListener("load", function(event) { //only initialize chart once window loads completely to avoid context issues
+    new Chart("myChart", {
+    type: "scatter",
+    data: {
+        datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues
+        }]
+    },
+    options: {
+        legend: {display: false},
+        scales: {
+        xAxes: [{ticks: {min: 0, max:150}}],
+        yAxes: [{ticks: {min: 0, max:120}}],
+        }
+    }
+    });
+    
+    new Chart("rnkpntChart", {
+    type: "scatter",
+    data: {
+        datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(255,255,0)",
+        data: rnkValues
+        }]
+    },
+    options: {
+        legend: {display: false},
+        scales: {
+        xAxes: [{ticks: {min: 0, max:balancingtime.length*placetime.length*autonpickup.length*autonmovepartofcycle.length*movepartofcycle.length}}],
+        yAxes: [{ticks: {min: 0, max:1,stepSize:1}}],
+        }
+    }
+    });
+});
 </script>
-
 <style>
 .card {
     display: flex;
